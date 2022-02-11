5757443f3d4dbe18f35a369da9ea7c9b3bc8db04
Jose <josewalls43@gmail.com>
2022-02-11 01:13:30 -0500

    Flix Part 2 initial commit

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Jose Paredes on 2/10/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
